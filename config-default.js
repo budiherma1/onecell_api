2cf6ebd508ba28732a11a00092bab3c16d6a1804
budianonym <budianonym@gmail.com>
2020-07-02 05:51:46 +0700

    adding config

---
+++ config-default.js
@@ -0,0 +1,11 @@
+const host = '103.146.203.15'
+const user = 'averoaco_one'
+const password = ''
+const database = 'averoaco_one'
+
+export default{
+    host: host,
+    user: user,
+    password: password,
+    database: database
+}
